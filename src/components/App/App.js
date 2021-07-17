c1aa33b978e3529103c309ce2f1269ec71e6b910
iTWise <xomerikigiorgi2@gmail.com>
2021-07-17 20:03:06 +0400

    last

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,65 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Example from "../ExampleClassComponent/ExampleFunctionComponent";
 import ExampleClass from "../ExampleClassComponent/ExampleClassComponent";
 import './App.css';
 
+function Message(props) {
+    return <p>{props.author}: {props.text}</p>
+}
 function App() {
-    const [showChild, setShowChild] = React.useState(true)
-    const messages = ['1', '2', '3', '4']
+
+    const [inputValue, setInputValue] = React.useState(``)
     const [messageList, setMessageList] = React.useState([])
-    const handleToggleShowChild = () => {
-        setShowChild(!showChild)
+    React.useEffect(() => {
+        if (messageList.length && messageList[messageList.length - 1].author !== AUTHOR.BOT) {
+            setTimeout(() => {
+                setMessageList((currentMessageList) => [
+                    ...currentMessageList,
+                    { text: `Sent`, author: AUTHOR.BOT }
+                ])
+            }, 1000)
+        }
+    }, [messageList])
+    const AUTHOR = {
+        ME: `Me`,
+        BOT: `Bot`
     }
-    const handleMessageList = () => <p>there is {messageList}</p>
+
+    // const handleMessage = () => <p>{messageList}</p>
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setMessageList((currentMessageList) => [
+            ...currentMessageList,
+            { text: inputValue, author: AUTHOR.ME }
+        ])
+        setInputValue(``)
+    }
+    const inputValueChange = (e) => {
+        setInputValue(e.target.value)
+    }
 
 
-    // Рендер с использованием фрагментов
-    // return (
 
-        
     return (
         <div className="app">
-            <div className="app__content">
-                <button onClick={handleToggleShowChild}>Toggle showChild</button>
-                {/* Условный рендер */}
-                {showChild ? <Example text="Welcome!" /> : null}
-                {showChild && <ExampleClass text="Welcome!" />}
-                <input className="bordered" placeholder="Инпут для примера" onChange={e => console.log(e)} />
-                <form className="email" onSubmit={handleMessageList}>
-                    <input type="text" className="input" onChange={e => setMessageList(e.target.value)} placeholder="Enter Your Email"></input>
-                    <input type="submit" value="Subscribe" onClick={handleMessageList} className="button">
-                    </input>
+            {messageList.map((message, index) => (
+                <Message key={index} author={message.author} text={message.text} />
+            ))}
+            <form className="email" onSubmit={handleSubmit}>
+                <input
+                    className="input"
+                    onChange={inputValueChange}
+                    value={inputValue}
+                    placeholder="Enter Message">
+                </input>
+                <input type="submit" className="button">
+                </input>
 
-                </form>
-                {/* messageList.map((message) => <div>{message}</div>) */}
-                
-                {console.log(messageList)}
-            </div>
+            </form>
+
+
         </div>
-    );
+    )
 }
 
 export default App;
