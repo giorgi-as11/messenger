fc43097b4eb1f61c4c6300804251441cd733dbc5
iTWise <xomerikigiorgi2@gmail.com>
2021-07-21 19:32:31 +0400

    latest

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,64 +2,100 @@ import React, { useEffect, useState } from 'react';
 import Example from "../ExampleClassComponent/ExampleFunctionComponent";
 import ExampleClass from "../ExampleClassComponent/ExampleClassComponent";
 import './App.css';
-
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { createTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+// import List from '@material-ui/core/List';
+// import ListItem from '@material-ui/core/ListItem'
+import AlignItemsList from './chat';
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: purple[500],
+        },
+        secondary: {
+            main: green[500],
+        },
+    },
+});
 function Message(props) {
-    return <p>{props.author}: {props.text}</p>
+    return <p key='id'>{props.author}: {props.text}</p>
 }
 function App() {
-
+    const chats = [{ id: 'aezakmi', name: 'nina' }, { id: 'kjkszpj', name: 'tata' }]
     const [inputValue, setInputValue] = React.useState(``)
     const [messageList, setMessageList] = React.useState([])
-    React.useEffect(() => {
-        if (messageList.length && messageList[messageList.length - 1].author !== AUTHOR.BOT) {
-            setTimeout(() => {
-                setMessageList((currentMessageList) => [
-                    ...currentMessageList,
-                    { text: `Sent`, author: AUTHOR.BOT }
-                ])
-            }, 1000)
-        }
-    }, [messageList])
     const AUTHOR = {
-        ME: `Me`,
-        BOT: `Bot`
-    }
-
-    // const handleMessage = () => <p>{messageList}</p>
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setMessageList((currentMessageList) => [
-            ...currentMessageList,
-            { text: inputValue, author: AUTHOR.ME }
-        ])
-        setInputValue(``)
+        ME: 'Me',
+        BOT: 'Bot'
     }
     const inputValueChange = (e) => {
         setInputValue(e.target.value)
     }
+    React.useEffect(() => {
+        if (messageList.length && messageList[messageList.length - 1].author !== AUTHOR.BOT) {
+            setTimeout(() => {
+                setMessageList((currentmessagelist) =>
+                    [...currentmessagelist,
+                    { text: 'text', author: AUTHOR.BOT }
+                    ])
+            }, 1000)
+        }
+    }, [messageList])
 
-
-
-    return (
-        <div className="app">
-            {messageList.map((message, index) => (
-                <Message key={index} author={message.author} text={message.text} />
-            ))}
-            <form className="email" onSubmit={handleSubmit}>
-                <input
-                    className="input"
-                    onChange={inputValueChange}
-                    value={inputValue}
-                    placeholder="Enter Message">
-                </input>
-                <input type="submit" className="button">
-                </input>
-
-            </form>
-
-
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setMessageList((currentmessagelist) =>
+            [...currentmessagelist,
+            { text: inputValue, author: `Me` }
+            ])
+        setInputValue(``)
+    }
+    return <div className='app'>
+        <div className="chat__members">
+            <div><AlignItemsList /></div>
+            <div className="chat">
+                {messageList.map((message, index) => {
+                    return <Message author={message.author} text={message.text} />
+                })}
+            </div>
         </div>
-    )
+
+        <form className="email" onSubmit={handleSubmit}>
+            {/* <input
+                className="input"
+                value={inputValue}
+                onChange={inputValueChange}
+                placeholder='Text here'
+            >
+            </input> */}
+            <TextField
+                // id="outlined-helperText"+
+                className="input"
+                value={inputValue}
+                onChange={inputValueChange}
+                label="Helper text"
+                autoFocus
+                required
+                defaultValue="Subscribe"
+                helperText="Some important text"
+                variant="outlined"
+            />
+
+            {/* <input type="submit" className="button">
+            </input>             */}
+                <Button variant="contained" onClick={handleSubmit} className="button" color="primary">
+                    Primary
+                </Button>
+
+        </form>
+
+
+    </div>
+
+
 }
 
 export default App;
